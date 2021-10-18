081624dee0c41e36112d2a40496e9aed52e2e206
BuildTools <unconfigured@null.spigotmc.org>
2021-10-18 13:49:11 -0400

    App header with font

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import {
 
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
+import Header from './components/AppHeader/Header';
 
 function App() {
   return (
+    <>
+    <Header />
     <Router>
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/profile' component={Profile} />
       </Switch>
     </Router>
+    </>
   );
 }
 
---
+++ src/components/AppHeader/Header.jsx
@@ -0,0 +1,21 @@
+import { AddBox } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import React, { useState } from 'react';
+
+const Header = ({ }) => {
+
+    const [title, setTitle] = useState('open-gram');
+
+    return (
+        <div className='app-header'>
+            <div className="app-header-title">{title}</div>
+            <div className="app-header-button">
+                <IconButton>
+                    <AddBox />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
