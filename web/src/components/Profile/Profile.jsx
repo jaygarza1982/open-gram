c1156cad916ea9aaf9cea21056bbd19a08e3cbc5
Jake Garza <jaygarza1982@gmail.com>
2021-11-18 17:20:16 -0500

    Fetches logged in profile on /profile if user is logged in

--- web/src/components/Profile/Profile.jsx
+++ web/src/components/Profile/Profile.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ProfileInfo from './ProfileInfo';
 import ProfilePosts from './ProfilePosts';
+import { useRecoilState } from 'recoil';
+import { userAtom } from '../../atoms';
+import useFetch from '../Hooks/useFetch';
 
 const Profile = ({ postURLs, postKeys }) => {
 
-    // TODO: If there are not specified urls or keys, we need to fetch the current user
+    const [user] = useRecoilState(userAtom);
+    const posts = useFetch(`/api/posts/list/${user}`);
+
     return (
         <div className='profile'>
             <ProfileInfo
@@ -15,7 +20,7 @@ const Profile = ({ postURLs, postKeys }) => {
                 name='Johnny'
             />
 
-            <ProfilePosts postURLs={postURLs} postKeys={postKeys} />
+            <ProfilePosts postURLs={postURLs || posts.map(p => p.content)} postKeys={postKeys || posts.map(p => p._id)} />
         </div>
     )
 }
